1dea798ed9cfda564c1f99b855488af313a6feca
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-14 12:22:46 -0500

    first commit

---
+++ src/test/java/com/anton/dev/servletfilterlistenerone/SpringBootServletFilterListenerOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anton.dev.servletfilterlistenerone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServletFilterListenerOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
